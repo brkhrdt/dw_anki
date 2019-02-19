94fff380df59f34b97f759f501b3db93535b91cb
Shawna Burkhardt <shawnabu@debian.debian>
2019-02-18 16:27:08 -0800

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(
     name = "dw_anki",
     version="0.1",
     packages=find_packages(),
+    install_requires=[
+        'lxml',
+        'requests',
+    ],
+    entry_points={
+        'console_scripts': [
+            'dw_anki = dw_anki.dw_anki:main',
+        ],
+    }
 )
